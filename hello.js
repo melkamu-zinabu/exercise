1898467e3128108af266143f79bdeb9b6c389a02
mz <zmelkamu918@gmail.com>
2022-04-04 22:33:18 +0300

    javascript while

--- hello.js
+++ hello.js
@@ -1,13 +1,5 @@
-console.log("melkamu");
-var me = 5;
-console.log(me)
-console.log(typeof(me));
-const arr = ["melkamu", 4, 3];
-console.log(arr[2]);
-
-function melkamu() {
-    console.log("my name is melkamu");
-    alert("wow wow");
-    console.log("boom");
+let x = 5;
+while (x <= 5 && x > 0) {
+    x--;
+    console.log(x);
 }
-melkamu();
