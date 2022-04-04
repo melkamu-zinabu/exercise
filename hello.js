d92470da5bb86410f8e7a2f07dfcdc3b7cd49f8f
mz <zmelkamu918@gmail.com>
2022-04-04 21:47:45 +0300

    javascript fun

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
 console.log("melkamu");
+var me = 5;
+console.log(me)
+console.log(typeof(me));
+const arr = ["melkamu", 4, 3];
+console.log(arr[2]);
+
+function melkamu() {
+    console.log("my name is melkamu");
+    alert("wow wow");
+    console.log("boom");
+}
+melkamu();
