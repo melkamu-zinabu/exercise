f44fa0f988ad44918f53eb75ac994927b04f79ab
mz <zmelkamu918@gmail.com>
2022-04-05 09:52:59 +0300

    jquery hide() method

--- hello.js
+++ hello.js
@@ -1,5 +1,25 @@
-let x = 5;
-while (x <= 5 && x > 0) {
-    x--;
-    console.log(x);
+console.log("melkamu");
+var me = 5;
+console.log(me)
+console.log(typeof(me));
+const arr = ["melkamu", 4, 3];
+console.log(arr[2]);
+
+function melkamu() {
+    console.log("my name is melkamu");
+    alert("wow wow");
+    console.log("boom");
 }
+melkamu();
+
+function product(a, b) {
+    if (a > b) {
+        return a * b;
+    } else if (
+        console.log(45)
+    )
+
+
+}
+let x = product(3, 9);
+console.log(x);
